77b58c4f989246de1e5e789590e116c8cc30fd3d
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-12-31 18:04:58 +0530

    Expression Tree

---
+++ Trees/bst_exp.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+struct tree
+{
+    char data;
+    struct tree *left, *right;
+};
+
+int top = -1;
+struct tree *stack[20];
+struct tree *root1;
+void push(struct tree *root)
+{
+    top = top + 1;
+    stack[top] = root;
+    printf("\nstack data: %p", stack[top]);
+}
+
+struct tree *pop()
+{
+
+    return (stack[top--]);
+}
+
+void inorder(struct tree *root)
+{
+
+    if (root == NULL)
+    {
+
+        return;
+    }
+    else
+
+    {
+        if (root != NULL)
+        {
+            inorder(root->left);
+        }
+        printf("%c", root->data);
+        inorder(root->right);
+    }
+}
+void operand(char b)
+{
+    struct tree *root;
+    root = (struct tree *)malloc(sizeof(struct tree));
+    root->data = b;
+
+    root->left = NULL;
+
+    root->right = NULL;
+    printf("\n%c", root->data);
+    printf("\noperend Address: %p ", root);
+    push(root);
+}
+
+void operators(char a)
+{
+    struct tree *root;
+    root = (struct tree *)malloc(sizeof(struct tree));
+    root->data = a;
+    printf(" \n operator data:%c ", root->data);
+    root->right = pop();
+    root->left = pop();
+    push(root);
+}
+int main(int argc, char **argv)
+{
+    int i = 0, p, k, ans;
+    char s[20];
+    printf("Enter the expression in postfix form \n");
+    scanf("%s", s);
+    while (s[i] != '\0')
+    {
+        if (isalnum(s[i]))
+        {
+            printf("\noperand:%c", s[i]);
+
+            operand(s[i]);
+        }
+        else
+        {
+
+            printf("\noperator:%c", s[i]);
+            operators(s[i]);
+        }
+        i++;
+    }
+    printf("\nThe inorder traversal of the tree is \n");
+    inorder(stack[top]);
+    return 0;
+}
